d8a061d92b13e119566cd5a9ab559fbc69f81ed3
angadsharma1016 <angadsharma1016@gmail.com>
2018-12-11 21:56:13 +0530

    fe

---
+++ static/js/angad.js
@@ -0,0 +1,19 @@
+$("#technical").on("click",()=>{
+    $("#technical").css("background-color", "#222")
+    $("#technical_h3").css("color", "white")
+})
+
+$("#management").on("click",()=>{
+    $("#management").css("background-color", "#222")
+    $("#management_h3").css("color", "white")
+})
+
+$("#design").on("click",()=>{
+    $("#design").css("background-color", "#222")
+    $("#design_h3").css("color", "white")
+})
+
+$("#documentation").on("click",()=>{
+    $("#documentation").css("background-color", "#222")
+    $("#documentation_h3").css("color", "white")
+})
