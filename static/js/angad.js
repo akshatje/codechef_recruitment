8ac30cab94e84a943bb69cd38b2ddc2aaffeb83a
angadsharma1016 <angadsharma1016@gmail.com>
2018-12-11 22:06:11 +0530

    fe

--- static/js/angad.js
+++ static/js/angad.js
@@ -1,19 +1,80 @@
+let technical=false, management = false, design = false, documentation = false;
+localStorage.clear();
+
+
+
 $("#technical").on("click",()=>{
-    $("#technical").css("background-color", "#222")
-    $("#technical_h3").css("color", "white")
+
+    if(!technical){
+        $("#technical").css("background-color", "#222")
+        $("#technical_h3").css("color", "white")
+        technical=!technical;
+    } else {
+        $("#technical").css("background-color", "#fafafa")
+        $("#technical_h3").css("color", "black")
+        technical=!technical;
+    }
+    localStorage.setItem("technical",technical)
+
+   
 })
 
 $("#management").on("click",()=>{
-    $("#management").css("background-color", "#222")
-    $("#management_h3").css("color", "white")
+    if(!management){
+        $("#management").css("background-color", "#222")
+        $("#management_h3").css("color", "white")
+        management=!management;
+
+    } else {
+        $("#management").css("background-color", "#fafafa")
+        $("#management_h3").css("color", "black")
+        management=!management;
+
+    }
+    localStorage.setItem("management",management)
+
 })
 
 $("#design").on("click",()=>{
-    $("#design").css("background-color", "#222")
-    $("#design_h3").css("color", "white")
+    if(!design){
+        $("#design").css("background-color", "#222")
+        $("#design_h3").css("color", "white")
+        design=!design;
+
+    } else {
+        $("#design").css("background-color", "#fafafa")
+        $("#design_h3").css("color", "black")
+        design=!design;
+
+    }
+    localStorage.setItem("design",design)
+
+    
 })
 
 $("#documentation").on("click",()=>{
-    $("#documentation").css("background-color", "#222")
-    $("#documentation_h3").css("color", "white")
+    if(!documentation) {
+        $("#documentation").css("background-color", "#222")
+        $("#documentation_h3").css("color", "white")
+        documentation=!documentation;
+
+    } else {
+        $("#documentation").css("background-color", "#fafafa")
+        $("#documentation_h3").css("color", "black")
+        documentation=!documentation;
+
+    }
+    localStorage.setItem("documentation",documentation)
+
+})
+
+
+$("#domain_sub").on("click", (e)=>{
+    e.preventDefault();
+    console.log({
+        technical,
+        design,
+        management,
+        documentation
+    })
 })
